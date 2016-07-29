aa666490f88267f1e1e4187298b54fb06403c09e
Di Tian <peacefultd@gmail.com>
2016-07-29 16:42:17 -0700

    :ghost: Initial commit

---
+++ HelloObjectiveC/HelloObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloObjectiveC
+//
+//  Created by Tian, Di on 7/29/16.
+//  Copyright © 2016 Tian, Di. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
